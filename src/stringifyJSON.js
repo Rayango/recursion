7192a28cd13b64c15d587ffc41691825e28b62a1
Ryan Ngo <rayango92@gmail.com>
2017-11-09 18:04:36 -0800

    finished conditions in which parameter is null, a number, or an object

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+  	return 'null';
+  } else if (typeof obj === 'number') {
+  	return obj.toString();
+  } else if (typeof obj === 'object' && obj !== null) {
+    var string = '{';
+    var lastProperty = Object.keys(obj)[Object.keys(obj).length - 1];
+    for (var property in obj) {
+  	  if (obj.hasOwnProperty(property)) {
+  	    string += '"' + property + '":';
+  
+  	    if (!isNaN(obj[property])) {
+  	      string += obj[property];
+  	    } else {
+  	    	string += '"' + obj[property] + '"';
+  	    }
+  
+  	    if (property !== lastProperty) {
+  	    	string += ',';
+  	    }
+  	  }	
+    }
+    string += '}';
+    return string;
+  } 
 };
