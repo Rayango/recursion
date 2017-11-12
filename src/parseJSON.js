7d9b4970cf7aa059acaff4811fe22766b1eb56ed
Ryan Ngo <rayango92@gmail.com>
2017-11-12 14:57:51 -0800

    added parsing for strings with escaped characters, fixed some bugs in object and array parsing

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,32 +4,40 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // search for [] and {} to determine if json string is of an array and/or object
-    // 
-  if (json === 'true') {
+  var trimmedJSON = json.trim();
+  if (trimmedJSON === 'true') {
     return true;
-  } else if (json === 'false') {
+  } else if (trimmedJSON === 'false') {
     return false;
-  } else if (json === 'null') {
+  } else if (trimmedJSON === 'null') {
     return null;
   }
-  if (json.indexOf('{') === -1 && json.indexOf('[') === -1) {
-  	return parseStringOrNumber(json);
-  } else if (json.indexOf('{') === 0) {
-    return parseObject(json);
-  } else if (json.indexOf('[') === 0) {
-  	return parseArray(json);
+  if (trimmedJSON.indexOf('{') === -1 && trimmedJSON.indexOf('[') === -1) {
+  	return parseStringOrNumber(trimmedJSON);
+  } else if (trimmedJSON.indexOf('{') === 0) {
+    return parseObject(trimmedJSON);
+  } else if (trimmedJSON.indexOf('[') === 0) {
+  	return parseArray(trimmedJSON);
   }
 
 };
 
 function parseStringOrNumber(json) {
   var parsedValue;
+  var escapedParsedValue = '';
   if (json.indexOf('"') === -1) {
     json.indexOf('.') === -1 ? parsedValue = parseInt(json) : parsedValue = parseFloat(json);	
   } else {
-  	parsedValue = json.replace(/"/g, '').trim();
+  	parsedValue = json.slice(1, json.length - 1);
+    if (parsedValue.indexOf('\\') !== -1) {
+      var index = 0;
+      while (parsedValue.indexOf('\\', index) !== -1) {
+        escapedParsedValue += parsedValue.slice(index, parsedValue.indexOf('\\', index)) + parsedValue.slice(parsedValue.indexOf('\\', index) + 1, parsedValue.indexOf('\\', index) + 2);
+        index = parsedValue.indexOf('\\', index) + 2;
+      }
+    }  
   }
-  return parsedValue;
+  return escapedParsedValue || parsedValue;
 }
 
 function parseObject(json) {
@@ -40,16 +48,23 @@ function parseObject(json) {
 
   while (jsonString.length) {
   	key = parseJSON(jsonString.slice(0, jsonString.indexOf(':')));
-  	if (jsonString.indexOf(',') !== -1) {
-  	  value = parseJSON(jsonString.slice(jsonString.indexOf(':') + 1, jsonString.indexOf(',')));
+    jsonString = jsonString.slice(jsonString.indexOf(':') + 1).trim();
+  	if (jsonString.indexOf('{') === 0) {
+      value = parseJSON(jsonString.slice(0, jsonString.lastIndexOf('}') + 1));
+      jsonString = jsonString.slice(jsonString.lastIndexOf('}') + 2);
+    } else if (jsonString.indexOf('[') === 0) {
+      value = parseJSON(jsonString.slice(0, jsonString.lastIndexOf(']') + 1));
+      jsonString = jsonString.slice(jsonString.lastIndexOf(']') + 2);
+    } else if (jsonString.indexOf(',') !== -1) {
+  	  value = parseJSON(jsonString.slice(0, jsonString.indexOf(',')));
       jsonString = jsonString.slice(jsonString.indexOf(',') + 1);	
   	} else {
-  	  value = parseJSON(jsonString.slice(jsonString.indexOf(':') + 1));	
+  	  value = parseJSON(jsonString);	
       jsonString = jsonString.slice(jsonString.length);
   	}  
-  	if (value.indexOf('{') === 0 || value.indexOf('[') === 0) {
-  	  value = parseJSON(value);	
-  	}
+  	// if (typeof value === 'string' && (value.indexOf('{') === 0 || value.indexOf('[') === 0)) {
+  	//   value = parseJSON(value);	
+  	// }
   	object[key] = value;
   }
 
